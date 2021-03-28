5ca31bacc45ef3287a8d23699c518f425d5923d9
Bylich <65658343+Taialin@users.noreply.github.com>
2021-03-28 21:56:16 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,68 +1,22 @@
+
 import logo from './logo.svg';
-import back from './back.svg';
+import back from './YJ4k5zVMjZA.jpg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useParams
-} from "react-router-dom";
-export default function ParamsExample() {
-  return (
-    <Router>
-      <div>
-        <h2>Accounts</h2>
-
-        <ul>
-          <li>
-            <Link to="/netflix">Netflix</Link>
-          </li>
-          <li>
-            <Link to="/zillow-group">Zillow Group</Link>
-          </li>
-          <li>
-            <Link to="/yahoo">Yahoo</Link>
-          </li>
-          <li>
-            <Link to="/modus-create">Modus Create</Link>
-          </li>
-        </ul>
-
-        <Switch>
-          <Route path="/:id" children={<Child />} />
-        </Switch>
-      </div>
-    </Router>
-  );
-}
-
-function Child() {
-  // We can use the `useParams` hook here to access
-  // the dynamic pieces of the URL.
-  let { id } = useParams();
-
-  return (
-    <div>
-      <h3>ID: {id}</h3>
-    </div>
-  );
-}
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <img src={back} alt=""/>
-      <img src={logo} className="App-logo" alt="logo" />
-      <p><a name="top"></a></p> 
-            <p>
-              TAP 
-            </p>
-        
+      <img src={logo} className="App-logo" alt="logo" />   
       </header>
+      <img src={back} className="App-back" alt=""/>
+      <Button className="App-link" variant="outline-dark" >TAP</Button>{' '}
+
     </div>
+    
+
     
     
   );
