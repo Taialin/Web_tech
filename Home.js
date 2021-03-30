a4b5e4624500942e221733442f92d51537ed7013
Bylich <65658343+Taialin@users.noreply.github.com>
2021-03-30 14:42:11 +0300

    Add files via upload

--- Home.js
+++ Home.js
@@ -1,13 +1,24 @@
 import React, {Component} from 'react';
 import './Home.css';
-import tamp from './tamp.svg';
+import gost from './gost.svg';
+import tamp from './tamp.png';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import minilogo from './minilogo.svg';
+
+
+
 
 class Home extends Component {
     render() {
         return (
+            
             <div className="Home"> 
       <header className="App-ack">
       <Container >
@@ -17,12 +28,18 @@ class Home extends Component {
     <Col><img className="App-tamp" src={tamp}  alt=""/></Col>
   </Row>
 </Container>
+
       </header>
-     
+      <nav class="navbar navbar-black ">
+      <a class="App-logosmall">
+      
+      </a>
+     </nav>
       </div>
       
         );
     }
+    
 }
 
 export default Home;
