38edc1e934b6b338146c343a2bffd15d0b025f96
Bylich <65658343+Taialin@users.noreply.github.com>
2021-04-01 12:51:48 +0300

    Add files via upload

--- Home.js
+++ Home.js
@@ -1,41 +1,63 @@
 import React, {Component} from 'react';
 import './Home.css';
-import gost from './gost.svg';
+import grodno from './grodno1.png';
 import tamp from './tamp.png';
-import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Button from 'react-bootstrap/Button';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
-import minilogo from './minilogo.svg';
-
-
+import Carousel from 'react-bootstrap/Carousel';
 
 
 class Home extends Component {
     render() {
         return (
-            
             <div className="Home"> 
+         
       <header className="App-ack">
-      <Container >
-  <Row>
-    <Col><img className="App-tamp" src={tamp}  alt=""/></Col>
-    <Col><img className="App-tamp" src={tamp}  alt=""/></Col>
-    <Col><img className="App-tamp" src={tamp}  alt=""/></Col>
-  </Row>
-</Container>
-
+        
       </header>
       <nav class="navbar navbar-black ">
       <a class="App-logosmall">
-      
       </a>
      </nav>
+     <Carousel className="Home-carusel">
+  <Carousel.Item>
+    <img
+      className="Home-gost"
+      src={tamp}
+      
+    />
+    <Carousel.Caption>
+    
+      <h3>First slide label </h3>
+      <img src={grodno} className="Home-grodno"/>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+     className="Home-gost"
+      src={tamp}
+      alt="Second slide"
+    />
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+     
+      className="Home-gost"
+      src={tamp}
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
       </div>
+      
       
         );
     }
